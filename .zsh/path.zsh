51c40af6a83b4c24f1270cb48b29c02d038b8cf3
exm8l <mark.powell@exness.com>
2024-07-29 16:30:25 +0300

    make user dynamic

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -1,10 +1,10 @@
-export PATH="/Users/mark/.local/bin:$PATH"
+export PATH="/Users/$USER/.local/bin:$PATH"
 export PATH="/opt/homebrew/bin:$PATH"
-export PATH="/Users/mark/go/bin:$PATH"
-export PATH="/Users/mark/Library/Python/3.9/bin:$PATH"
+export PATH="/Users/$USER/go/bin:$PATH"
+export PATH="/Users/$USER/Library/Python/3.9/bin:$PATH"
 export PATH="/opt/homebrew/opt/mysql-client/bin:$PATH"
 # MVN installation version
-export M2_HOME="/Users/mark/.local/share/mvn/apache-maven-3.8.5"
+export M2_HOME="/Users/$USER/.local/share/mvn/apache-maven-3.8.5"
 export PATH="${M2_HOME}/bin:$PATH"
 
 # pyenv
